8ea82a2c6a7f04fdbf2852e9a994bf97ab5ed93b
Charlie Smith <charlie@chuckus.nz>
2019-09-29 17:50:37 +0700

    Send random image each time

--- jeremybot/__init__.py
+++ jeremybot/__init__.py
@@ -2,11 +2,15 @@ import asyncio
 import os
 
 import discord
+import requests
 import pickledb
 
 # TODO: probably should use sqlite3 here to be able to deal with multiple guilds for single bot
 db = pickledb.load('example.db', False)
 db.set("admins", "ruckusmaker")
+
+
+IMAGE_URL = "https://picsum.photos/200"
 
 
 class MyClient(discord.Client):
@@ -21,16 +25,21 @@ class MyClient(discord.Client):
 
     async def spam_message(self):
         # TODO: wait for on_ready event before continuing
-        await asyncio.sleep(3)
+        await self.wait_until_ready()
         print(self.guilds)
         print(self.guilds[0].channels)
         while True:
             guild = self.guilds[0]
             channel = guild.channels[1]
-            await channel.send("test")
+            img_data = requests.get(IMAGE_URL).content
+            with open('image_name.jpg', 'wb') as handler:
+                handler.write(img_data)
+            await channel.send("test", file=discord.File(open("image_name.jpg", "rb")))
             # TODO: download image from url and send
             # TODO: have local cache for image
             # TODO: make sleep periodic configurable by user admin
+            # TODO: change display name on every post
+            # TODO: source display names from sqlite3
             await asyncio.sleep(5)
 
 
